c4ad3333acfd408561af62b892de30c2d47c267e
Tauhous <tauhous@outlook.com>
2026-07-10 13:29:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const calendar = document.getElementById("calendar");
 
+for(let day = 1; day <= 31; day++){
+
+    let box = document.createElement("div");
+    box.className = "day";
+
+    box.innerHTML = `
+        <h3>${day}</h3>
+        <img 
+          class="icon"
+          src="assets/icons/game.png"
+          onclick="openItem(${day})">
+    `;
+
+    calendar.appendChild(box);
+}
+
+
+function openItem(day){
+    alert("Opening item for day " + day);
+}
